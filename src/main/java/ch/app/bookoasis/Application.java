a6592f56811a28a31e33c0599d2adac331289cd6
Enrico-Proietto <enrico.proiettog@gmail.com>
2023-11-02 11:23:41 +0100

    initial commit

---
+++ src/main/java/ch/app/bookoasis/Application.java
@@ -0,0 +1,24 @@
+package ch.app.bookoasis;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "bookoasis", variant = Lumo.DARK)
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
